4cc99bc3dc8683d656cbf11b5bc8c2d6c8b18702
Maysara-nagy <maysara.nagy@gmail.com>
2022-10-17 10:28:56 -0700

    lol

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int ch = 'a';
+	int chUPPER = 'A';
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (chUPPER <= 'Z')
+        {
+                putchar(chUPPER);
+                chUPPER++;
+        }
+
+	putchar('\n');
+	return (0);
+}
