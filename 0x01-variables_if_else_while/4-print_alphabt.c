00100004b7fc652ff07a7e6c153922327aa09ee5
Maysara-nagy <maysara.nagy@gmail.com>
2022-10-17 10:45:51 -0700

    lol

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int ch = 'a';
+	int chUPPER = 'A';
+
+	while (ch <= 'z')
+	{
+		if ((ch != 'q' && ch != 'e') && ch <= 'z')
+			putchar(ch);
+			ch++;
+	}
+	putchar('\n');
+	return (0);
+}
