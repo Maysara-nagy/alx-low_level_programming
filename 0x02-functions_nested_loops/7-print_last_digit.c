c9b1946c529ac1479a83abe6f0c745654f7eb9ce
Maysara-nagy <maysara.nagy@gmail.com>
2022-10-19 07:09:46 -0700

    my commit to publish it

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ * @n: number to print his last digit
+ * Return: alwyas0
+ */
+
+int print_last_digit(int n)
+{
+	int sum = n % 10;
+
+	return (sum);
+}
