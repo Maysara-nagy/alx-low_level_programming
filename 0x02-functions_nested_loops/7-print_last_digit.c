a42fa79bb552583cd914764e8b17d046bda9968e
Maysara-nagy <maysara.nagy@gmail.com>
2022-10-19 08:34:54 -0700

    my commit to publish it

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,11 @@ int print_last_digit(int n)
 	int sum = n % 10;
 
 	if (sum < 0)
-		sum *= -1;
-	_putchar(sum + '0');
-	return (_putchar(sum + '0'));
+		_putchar(-sum + '0');
+		return (_putchar(-sum + '0');)
+	else
+	{
+		_putchar(sum + '0');
+		return (_putchar(sum + '0'));
+	}
 }
