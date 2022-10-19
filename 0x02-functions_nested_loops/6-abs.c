97b8c2dcffa95233fb8936ef1d4034743d0627d8
Maysara-nagy <maysara.nagy@gmail.com>
2022-10-19 07:03:17 -0700

    my commit to publish it

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value of an integer
+ * Return: always n
+ * @t: parameter
+ */
+
+int _abs(int t)
+{
+	int sum = t * -1;
+
+	if (t == 0)
+		return (_putchar('0'));
+	else if (t < 0)
+		return (_putchar(sum + '0'));
+	else
+		return (_putchar(t + '0'));
+}
