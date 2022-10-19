76eb69996907071429df93c763a456820773121d
Maysara-nagy <maysara.nagy@gmail.com>
2022-10-19 06:12:12 -0700

    my commit to publish it

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - printf alphabet in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int ch = 'a';
+	int i = 0;
+
+	while (i < 10)
+	{
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+	}
+	_putchar('\n');
+}
