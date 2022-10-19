ae69546cdaa0ceab8540b9d75d8f1f086f66b826
Maysara-nagy <maysara.nagy@gmail.com>
2022-10-19 05:59:48 -0700

    my commit to publish it

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - printf alphabet in lowercase
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar('ch');
+		ch++;
+	}
+	_putchar('\n');
+	return (0);
+}
