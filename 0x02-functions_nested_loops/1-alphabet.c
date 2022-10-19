e18be9d31a16ebd457c26d2d962acb2ef13d0947
Maysara-nagy <maysara.nagy@gmail.com>
2022-10-19 06:01:55 -0700

    my commit to publish it

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,6 @@
 
 /**
  * print_alphabet - printf alphabet in lowercase
- * Return: always 0
  */
 void print_alphabet(void)
 {
@@ -14,5 +13,4 @@ void print_alphabet(void)
 		ch++;
 	}
 	_putchar('\n');
-	return (0);
 }
