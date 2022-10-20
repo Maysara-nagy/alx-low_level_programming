ec9ebbbea5bd2556671cb297cb2f39c29b2b62bb
Maysara-nagy <maysara.nagy@gmail.com>
2022-10-19 18:53:17 -0700

    my commit to publish it

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add to intergers
+ * Return: the result
+ * @num1: num1
+ * @num2: num2
+ */
+
+int add(int num1, int num2)
+{
+	int result = num1 + num2;
+
+	return (result);
+}
