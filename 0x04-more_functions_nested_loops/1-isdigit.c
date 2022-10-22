99c8489ec0c4df9c5583de42c209d3e0eaa56ea6
Maysara-nagy <maysara.nagy@gmail.com>
2022-10-21 18:25:56 -0700

    my commit to publish it

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - function to check if it digit or not
+ * Return: 1 i c i digit
+ * @c: the digit
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
