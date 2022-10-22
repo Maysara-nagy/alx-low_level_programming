9491d539de5f0f1853fee0bac00caf2d933d34e9
Maysara-nagy <maysara.nagy@gmail.com>
2022-10-21 18:51:50 -0700

    my commit to publish it

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,9 +10,10 @@ void print_most_numbers(void)
 
 	while (i <= 9)
 	{
-		if (i == 2 || i == 4)
-			continue;
-		_putchar(i + '0');
+		if (!(i == 2 || i == 4))
+		{
+			_putchar(i + '0');
+		}
 		i++;
 	}
 	_putchar('\n');
