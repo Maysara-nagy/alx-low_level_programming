f3b7fd6f7756f578af4ecfb057d60173635b896d
Maysara-nagy <maysara.nagy@gmail.com>
2022-10-24 21:34:13 -0700

    my commit to publish it

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,12 +10,12 @@ void print_rev(char *s)
 	int i = 0;
 	int j = i - 1;
 
-	while (*(s + i) != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 		while (j >= 0)
 		{
-			_putchar(*(s + j));
+			_putchar(s[j]);
 			j--;
 		}
 	}
