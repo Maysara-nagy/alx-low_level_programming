f19a449ea602a22d939779ca56e2b2cb2027dc83
Maysara-nagy <maysara.nagy@gmail.com>
2022-11-02 21:37:24 -0700

    my commit to publish it

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - bla bla
+ * @s: string
+ * Return: valiue
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\n')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
