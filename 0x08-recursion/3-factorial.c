3357059ec985cb536df6dc94d15644adf9e1176c
Maysara-nagy <maysara.nagy@gmail.com>
2022-11-02 21:43:50 -0700

    my commit to publish it

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - bla bla
+ * @n: number
+ * Return: void
+ */
+
+int factorial(int n)
+{
+	if (n <= 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
