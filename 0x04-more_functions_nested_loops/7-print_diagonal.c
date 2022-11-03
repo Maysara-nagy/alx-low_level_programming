f16c0aa9559cf47ea89d68298377b262b1920cea
Maysara-nagy <maysara.nagy@gmail.com>
2022-11-02 19:57:34 -0700

    my commit to publish it

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print / diagonal
+ * @n: number of rows
+ */
+
+void print_diagonal(int n)
+{
+	int x, y;
+
+	x = 1;
+	y = 1;
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		while (x <= n)
+		{
+			while (y <= x)
+			{
+				_putchar(' ');
+				y++;
+			}
+			x++;
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
