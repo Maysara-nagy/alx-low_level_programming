6a93bae5c3c909fde67a075564ff4403f565d866
Maysara-nagy <maysara.nagy@gmail.com>
2022-11-05 14:51:00 -0700

    my commit to publish it

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,9 +7,9 @@
  * Return: always 0
  */
 
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
 	(void) argv;
-	printf("%i\n", argc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
