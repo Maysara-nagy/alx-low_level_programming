1ed5f3228f14e9fd8b30176d19a3cf0b021f6e52
Maysara-nagy <maysara.nagy@gmail.com>
2022-11-05 14:46:49 -0700

    my commit to publish it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: count
+ * @argv: array of pointers ot string
+ * Return: always 0
+ */
+
+int main(int argc, int *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+}
