f74694c23413fa6aeb722ec17558690be6652560
Maysara-nagy <maysara.nagy@gmail.com>
2022-11-05 15:06:35 -0700

    my commit to publish it

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: count
+ * @argv: array of pointers to string
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc == 3)
+	{
+		a = atoi(argv[0]);
+		b = atoi(argv[1]);
+		printf("%d\n", a * b);
+		return (0);
+	}
+	printf("Errorn\n");
+	return (1);
+}
