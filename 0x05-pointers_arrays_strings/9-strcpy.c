315a6af7004863c360eaafe7d03ff6b04b2347da
Maysara-nagy <maysara.nagy@gmail.com>
2022-11-22 11:00:39 -0800

    my commit to publish it

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcpy - function to copy strings
+ * @dest: the copy destination
+ * @src: the string wanted to get copy
+ * Retrun: always fking dest
+ * /
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
