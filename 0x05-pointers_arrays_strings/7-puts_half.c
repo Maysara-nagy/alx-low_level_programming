01ecca53f5d1c37b08cee87d4efd04e3b159681e
Maysara-nagy <maysara.nagy@gmail.com>
2022-11-22 09:55:09 -0800

    my commit to publish it

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - function to primts half of a string
+ * @str: the array
+ */
+
+void puts_half(char *str)
+{
+	int length;
+	int i = 0;
+	int n, j;
+
+	while (*(str + i) != '\0')
+	{
+		length++;
+		i++;
+	}
+	n = (length - 1) / 2;
+	j = length / 2;
+	if (length % 2 != 0)
+	{
+		_putchar('n');
+	}
+	else
+	{
+		while (j <= i)
+		{
+			_putchar(*(str + j));
+			j++;
+		}
+	}
+}
