bad8925d18f8b3bf859e5d0765c8a5959acc1ee3
Maysara-nagy <maysara.nagy@gmail.com>
2022-11-23 09:11:08 -0800

    my commit to publish it

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,8 +10,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = &dog;
-	d->name;
-	d->age;
-	d->owner;
+	if (d != NULL)
+	{	
+		d = &dog;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
