2ffcf91a48493ec16e382f0e226b52081fdeb4ce
Maysara-nagy <maysara.nagy@gmail.com>
2022-11-23 09:09:35 -0800

    my commit to publish it

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog: function to init the struct dog
+ * @d: an element
+ * @name: name element
+ * @age: age
+ * @owner: owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = &dog;
+	d->name;
+	d->age;
+	d->owner;
+}
