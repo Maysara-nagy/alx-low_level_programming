5007bf65dd905468348978b0e1d42cafc18abec1
Maysara-nagy <maysara.nagy@gmail.com>
2022-11-23 09:37:47 -0800

    my commit to publish it

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,8 +12,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	if (new_dog != NULL)
 	{
-		new_dog.name = name;
-		new_dog.age = age;
-		new_dog.owner = owner;
+		new_dog->name = name;
+		new_dog->age = age;
+		new_dog->owner = owner;
 	}
 }
