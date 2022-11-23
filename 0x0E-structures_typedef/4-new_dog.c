c8c545ff71cdb2da18c580a9efff2897b020b222
Maysara-nagy <maysara.nagy@gmail.com>
2022-11-23 09:37:06 -0800

    my commit to publish it

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - function to create new dog
+ * @name: argu
+ * @age: argu
+ * @owner: argu
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (new_dog != NULL)
+	{
+		new_dog.name = name;
+		new_dog.age = age;
+		new_dog.owner = owner;
+	}
+}
