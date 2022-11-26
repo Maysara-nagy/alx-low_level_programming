45d1f635a1db73f7afd4590f0558af57364cd9ea
Maysara-nagy <maysara.nagy@gmail.com>
2022-11-26 10:21:48 -0800

    my commit to publish it

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include "stdlib.h"
+
+/**
+ * free_dog - function to free dogs
+ * @d: structure
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d);
+	free(d->name);
+	free(d->owner);
+}
