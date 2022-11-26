f895d8c4e94d8f0e2beebc304b35722f3cc2b6f2
Maysara-nagy <maysara.nagy@gmail.com>
2022-11-25 17:41:49 -0800

    my commit to publish it

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * first - function that will be exuted before main funciton
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
