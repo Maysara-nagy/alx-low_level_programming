9c35be6c521fa470405f65493bfb7a994a219941
Maysara-nagy <maysara.nagy@gmail.com>
2022-11-27 04:04:22 -0800

    my commit to publish it

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%u", va_arg(args, int));
+		printf("%d", va_arg(args, int));
 		if (separator && i != n - 1)
 		{
 			printf("%s", separator);
