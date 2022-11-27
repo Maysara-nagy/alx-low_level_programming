d78e87dc88464aa05ea3e4c295b0be9dc1ee917d
Maysara-nagy <maysara.nagy@gmail.com>
2022-11-27 03:51:49 -0800

    my commit to publish it

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - function to print numbers
+ * @n: number of arguments
+ * @separator: string to seperate between numbers
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+	if (separator)
+	{
+		for (i = 0; i < n; i++)
+		{
+			printf("%s%u", separator, n);
+		}
+		va_end(args);
+		printf("\n");
+	}
+}
