951374a7cc72badcf72e6879581b2831739eb552
Maysara-nagy <maysara.nagy@gmail.com>
2022-12-09 10:09:02 -0800

    my commit to publish it

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - function to read a text file and prints it to posix standard output
+ * @filename: the file name or path of the file
+ * @letters: number of letter the programe will read
+ * Retrun: 0 if the file can not be opened or read | 0 if the fileanme = null | 0 if write fails
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, i, y;
+	char *buf;
+
+	fd = open(filename, O_CREATE | O_RDWR);
+	if (fd < 0 | filename == NULL)
+		return (0);
+	else
+	{
+		buf = malloc(sizeof(char) * letters);
+		if (!buff)
+			return (0);
+		i = read(fd, buf, letters);
+		if (i < 0)
+			return (0);
+		buf[i] = '\0';
+		close(fd);
+		y = write(STDOUT_FILENO, buf, i);
+		if (y < 0)
+		{
+			free (buf);
+			return (0);
+		}
+		free (buf)
+		return (y);
+	}
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+this is my file i/o dir it talk about system calls like open() write() read() close()
