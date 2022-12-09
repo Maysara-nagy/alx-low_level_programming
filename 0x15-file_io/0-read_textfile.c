9b44195ad5e09fab986066a92b4beab25d09ca4a
Maysara-nagy <maysara.nagy@gmail.com>
2022-12-09 10:14:09 -0800

    my commit to publish it

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,29 +15,31 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, i, y;
+	int fd;
+	int i, y;
 	char *buf;
-
-	fd = open(filename, O_RDONLY);
-	if (fd < 0 | filename == NULL)
+	if (!filename)
 		return (0);
-	else
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+	i = read(fd, buf, letters);
+	if (i < 0)
 	{
-		buf = malloc(sizeof(char) * letters);
-		if (!buf)
-			return (0);
-		i = read(fd, buf, letters);
-		if (i < 0)
-			return (0);
-		buf[i] = '\0';
-		close(fd);
-		y = write(STDOUT_FILENO, buf, i);
-		if (y < 0)
-		{
-			free (buf);
-			return (0);
-		}
-		free (buf)
-		return (y);
+		free(buf);
+		return (0);
 	}
+	buf[i] = '\0';
+	close(fd);
+	y = write(STDOUT_FILENO, buf, i);
+	if (y < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	return (y);
 }
