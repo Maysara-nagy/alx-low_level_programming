60972e3b7414ee624f4107fad41af13c899cd8c6
Maysara nagy <81137772+Maysara-nagy@users.noreply.github.com>
2023-08-18 05:40:53 +0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,26 +1,21 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_square - Print square line dependent on the integer n.
- * @n : The number of lines using '#' characters to use per row and column
- * Return: Void.
- */
-void print_square(int n)
-{
-	int x;
-	int y;
+ * print_square - print a squar using the character #
+ *
+ * @size: is the size of the square
+ *
+ * Return: always 0 (success)
+*/
 
-	for (y = 0; y < n; y++)
-	{
-		for (x = 0; x < n; x++)
-		{
-			_putchar('#');
-		}
-		_putchar('\n');
-	}
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
+void print_square(int size)
+{
+        int row, column;
+
+        for (row = 1; row <= size; row++)
+        {
+                for (column = 1; column <= size; column++)
+                        _putchar('#');
+          _putchar('\n');
+        }
 }
